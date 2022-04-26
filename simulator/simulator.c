0f1fd0e8f2805e2c20207e0d5146158b15d6d3cb
bidpl <binhminh292@gmail.com>
2022-04-26 03:12:50 -0500

    Started print instructions in human format

--- simulator/simulator.c
+++ simulator/simulator.c
@@ -6,6 +6,11 @@
 #define BUFF_LEN 127
 #define PATH_BUFF_LEN 255
 #define LAST_MEM_ADDR 0xFDFF
+#define DUMP_LEN 10
+
+#define MAX_INSTR_LEN 35
+
+const char *INSTR[] = {"BR", "ADD", "LD", "ST", "JSR", "AND", "LDR", "STR", "RTI", "NOT", "LDI", "STI", "JMP", "RET", "LEA", "TRAP", "JSRR"};
 
 int main(int argc, char *argv[]) {
     char readBuff[BUFF_LEN];
@@ -37,10 +42,78 @@ int main(int argc, char *argv[]) {
         if(strcmp(command, "file") == 0 || strcmp(command, "f") == 0) {
             load_binary(lc3cpu, option1);
             continue;
+
         } else if(strcmp(command, "list") == 0 || strcmp(command, "l") == 0) {
+            // printf("List instructions @ PC (defualt), address, or label\n");
+            __UINT16_TYPE__ memAddr;
+
+            if(numRead == 1) {
+                memAddr = lc3cpu->PC;
+            } else {
+                // TODO make this not case sensitive
+                memAddr = get_sym_addr(lc3cpu, option1);
+                
+                if(memAddr == 0) {
+                    if(sscanf(option1, "%*c %hx", &memAddr) != 1) {
+                        printf("Invalid label/memory address\n");
+                        continue;
+                    }
+                }            
+            }
+
+            for(int i = 0; i < DUMP_LEN; i++) {
+                printf("x%04X   ", memAddr + i);
+
+                print_instr(lc3cpu, memAddr + i);
+            }
+             
+            printf("\n");  
+        } else if(strcmp(command, "dump") == 0 || strcmp(command, "d") == 0) {
+            // printf("Dump memory @ PC (defualt), address, or label");
+
+            __UINT16_TYPE__ memAddr;
+
+            if(numRead == 1) {
+                memAddr = lc3cpu->PC;
+            } else {
+                // TODO make this not case sensitive
+                memAddr = get_sym_addr(lc3cpu, option1);
+                
+                if(memAddr == 0) {
+                    if(sscanf(option1, "%*c %hx", &memAddr) != 1) {
+                        printf("Invalid label/memory address\n");
+                        continue;
+                    }
+                }            
+            }
+                
+            for(int i = 0; i < DUMP_LEN; i++) {
+                dumpMem(lc3cpu, memAddr + i);
+            }    
+            printf("\n");
+
+        } else if(strcmp(command, "translate") == 0 || strcmp(command, "t") == 0) {
+            // printf("Show value of label + print contents");
+            if(numRead != 2) {
+                printf("Invalid arguments (invalid # of args)\n");
+                continue;
+            }
+
+            __UINT16_TYPE__ memAddr = get_sym_addr(lc3cpu, option1);
+
+            if(memAddr == 0) {
+                printf("Could not find label: %s\n", option1);
+            }else {
+                printf("%s: x%04X\n", option1, memAddr);
+                dumpMem(lc3cpu, memAddr);
+            }
             
+        } else if(strcmp(command, "printregs") == 0 || strcmp(command, "p") == 0) {
+            // printf("Print registers and current instruction\n");
+            printregs(lc3cpu);
+
         } else if(strcmp(command, "quit") == 0 || strcmp(command, "q") == 0) {
-            printf("Are you sure you want to quit? y to confirm: ");
+            printf("Are you sure you want to quit? [y]: ");
             scanf("%s", command);
 
             if(strcmp(command, "y") == 0) {
@@ -50,6 +123,8 @@ int main(int argc, char *argv[]) {
                 printf("Quit canceled\n");
                 continue;
             }
+        } else if(strcmp(command, "q!") == 0) {
+            break;
         }
 
     }
@@ -67,6 +142,12 @@ int main(int argc, char *argv[]) {
 CPU * create_CPU(){
     CPU* newCPU = (CPU*) malloc(sizeof(CPU));
 
+    // Initialize values to 0
+    for(int i = 0; i < 8; i++) {
+        newCPU->regfile[i] = 0;
+    }
+    newCPU->numSymbols = 0;
+
     return newCPU;
 }
 
@@ -78,6 +159,11 @@ CPU * create_CPU(){
  * @return nothing
  */
 void destroy_CPU(CPU* cpu) {
+    int i;
+    for(i = 0; i < cpu->numSymbols; i++) {
+        free(cpu->symbols[i]);
+    }
+
     free(cpu);
 }
 
@@ -96,6 +182,7 @@ int load_binary(CPU* cpu, char* filename){
     char sym_filename[PATH_BUFF_LEN];
 
     __UINT16_TYPE__ readBuff;
+    char buff[BUFF_LEN];
 
     parse_filename(filename, bin_filename, sym_filename);
 
@@ -120,7 +207,7 @@ int load_binary(CPU* cpu, char* filename){
 
     cpu->PC = readBuff;
     memAddr = readBuff;
-    printf("Starting at x%04x\n", readBuff);
+    // printf("Starting at x%04x\n", readBuff);
 
     // Keep on reading file and appending to memory;
     while(!feof(bin_fp)) {
@@ -145,7 +232,7 @@ int load_binary(CPU* cpu, char* filename){
         readBuff = le_to_be(readBuff);
         
         // Write word to memory
-        printf("x%04x: x%04x\n", memAddr, readBuff);
+        // printf("x%04x: x%04x\n", memAddr, readBuff);
         cpu->memory[memAddr] = readBuff;
         memAddr++;
     }
@@ -161,6 +248,36 @@ int load_binary(CPU* cpu, char* filename){
     }
 
     // TODO Load sym table here
+    // Flush symbol table header
+    fgets(buff, BUFF_LEN, sym_fp); // Symbol table
+    fgets(buff, BUFF_LEN, sym_fp); // Scope level 0:
+    fgets(buff, BUFF_LEN, sym_fp); //	Symbol Name       Page Address
+    fgets(buff, BUFF_LEN, sym_fp); //	----------------  ------------
+
+    // Read and import symbol table
+    while(fgets(buff, BUFF_LEN, sym_fp)) {
+        char symName[20];
+        __UINT16_TYPE__ addr;
+
+
+        // Read in line + parse
+        if(sscanf(buff, "%*s %s %hx", symName, &addr) != 2) {
+            if(strcmp(buff, "\n") != 0) {
+                printf("Bad symbol table line: %s\n", buff);
+            }
+        }
+
+        // Add to symbol table
+        cpu->symbols[cpu->numSymbols] = (char *) malloc(strlen(symName) + 1);
+        strcpy(cpu->symbols[cpu->numSymbols], symName);
+        cpu->symAddr[cpu->numSymbols] = addr;
+        cpu->numSymbols++;
+    }
+
+    // Debug print symbol table
+    // for(int i = 0; i < cpu->numSymbols; i++) {
+    //     printf("%s: %04x\n", cpu->symbols[i], get_sym_addr(cpu, cpu->symbols[i]));
+    // }
     
     fclose(sym_fp);
 
@@ -232,3 +349,186 @@ __UINT16_TYPE__ le_to_be(__UINT16_TYPE__ input) {
     return ((input << 8) | (input >> 8)) & 0xFFFF;
 }
 
+
+/**
+ * @brief get_sym_addr: takes symbol and returns corresponding address
+ * 
+ * @param CPU* lc3cpu
+ * @param char* symName
+ * @return uint16_t address (0 if symbol doesn't exist)
+ */
+
+__UINT16_TYPE__ get_sym_addr(CPU* lc3cpu, char* symName) {
+    int i;
+    for(i = 0; i < lc3cpu->numSymbols; i++) {
+        if(strcmp(symName, lc3cpu->symbols[i]) == 0) {
+            return lc3cpu->symAddr[i];
+        }
+    }
+
+    return 0;
+}
+
+/**
+ * @brief get_addr_sym: takes address and returns pointer to address name
+ * 
+ * @param CPU* lc3cpu
+ * @param uint16_t addr
+ * @return char* sym (NULL if address doesn't have symbol)
+ */
+
+char* get_addr_sym(CPU* lc3cpu, __UINT64_TYPE__ addr) {
+    int i;
+    for(i = 0; i < lc3cpu->numSymbols; i++) {
+        if(addr == lc3cpu->symAddr[i]) {
+            return lc3cpu->symbols[i];
+        }
+    }
+
+    return 0;
+}
+
+/**
+ * @brief dumpMem: takes addr and prints contents of address
+ * 
+ * @param CPU* lc3cpu
+ * @param uint16_t addr
+ * @return nothing
+ */
+
+void dumpMem(CPU* lc3cpu, __UINT16_TYPE__ addr) {
+    printf("x%04X: x%04X\n", addr, lc3cpu->memory[addr]);
+}
+
+
+/**
+ * @brief printregs: prints registers (R0-R7, PSR) and current instruction
+ * 
+ * @param CPU* lc3cpu
+ * @return nothing
+ */
+
+void printregs(CPU* lc3cpu) {
+    
+    // Print PC + PSR
+    __UINT16_TYPE__ PSR = lc3cpu->memory[0xFFFC];
+    char cc;
+
+    if(PSR & 0b100) {
+        cc = 'n';
+    } else if(PSR & 0b010) {
+        cc = 'z';
+    } else {
+        cc = 'p';
+    }
+
+    printf("PC: x%04x   PSR: x%04X  (CC=%c)\n", lc3cpu->PC, PSR, cc);
+
+    // Print R0-R7
+    printf("R0: x%04X   R1: x%04X   R2: x%04X   R3: x%04X   \n", lc3cpu->regfile[0], lc3cpu->regfile[1], lc3cpu->regfile[2], lc3cpu->regfile[3]);
+    printf("R4: x%04X   R4: x%04X   R6: x%04X   R7: x%04X   \n\n", lc3cpu->regfile[4], lc3cpu->regfile[5], lc3cpu->regfile[6], lc3cpu->regfile[7]);
+
+    // TODO decode instruction
+    printf("x%04X   ", lc3cpu->PC);
+
+    print_instr(lc3cpu, lc3cpu->PC);
+
+    printf("\n");
+}
+
+
+/**
+ * @brief print_instr: prints instruction at a memory address in human readable format
+ * 
+ * @param CPU* lc3cpu
+ * @param uint16_t memAddr
+ * @return nothing
+ * 
+ */
+
+void print_instr(CPU* lc3cpu, __UINT16_TYPE__ memAddr) {
+    __UINT16_TYPE__ instruction = lc3cpu->memory[memAddr];
+    int opcode = instruction >> 12;
+
+    if(instruction == 0) {
+        printf("NOP\n");
+        return;
+    }
+
+    // If BR
+    if(opcode == 0) {
+        __INT16_TYPE__ PCoffset9 = instruction & 0x1FF;
+        // SEXT PCoffset9
+        if(instruction & 0x100) {
+            PCoffset9 |= 0xFE00;
+        }
+
+        char ccBits[4] = {0, 0, 0, 0};
+
+        // n bit
+        if(instruction & 0x0800) {
+            strcat(ccBits, "n");
+        }
+
+        // z bit
+        if(instruction & 0x0400) {
+            strcat(ccBits, "z");
+        }
+
+        // p bit
+        if(instruction & 0x0200) {
+            strcat(ccBits, "p");
+        }
+
+        // Check if there is an associated symbol
+        char* symbol = get_addr_sym(lc3cpu, memAddr + 1 + PCoffset9);
+        if(symbol != NULL) {
+            printf("BR%s %s\n", ccBits, symbol);
+        } else {
+            printf("BR%s #%d\n", ccBits, PCoffset9);
+        }
+        
+        return;
+    }
+
+    // If ADD or AND
+    if(opcode == 0b0001 || opcode == 0b0101) {
+        // if instruction[5]
+        if(instruction & 0b100000) {
+            __INT16_TYPE__ imm5 = instruction & 0b11111;
+            
+            // SEXT imm5
+            if(instruction & 0b10000) {
+                imm5 |= 0xFFE0;
+            }
+            
+            
+            printf("%s R%1d, R%1d, #%d", INSTR[opcode], (instruction >> 9) & 0b111, (instruction >> 6) & 0b111, imm5);
+        } else {
+            printf("%s R%1d, R%1d, R%1d", INSTR[opcode], (instruction >> 9) & 0b111, (instruction >> 6) & 0b111, instruction & 0b111);
+        }
+
+        printf("\n");
+        return;
+    }
+
+    // LD, LDI, LEA, ST, STI
+    if(opcode == 0b0010 || opcode == 0b1010 || opcode == 0b1110 || opcode == 0b0011 || opcode == 0b1011) {
+        __INT16_TYPE__ PCoffset9 = instruction & 0x1FF;
+        // SEXT PCoffset9
+        if(instruction & 0x100) {
+            PCoffset9 |= 0xFE00;
+        }
+
+        // Check if there is an associated symbol
+        char* symbol = get_addr_sym(lc3cpu, memAddr + 1 + PCoffset9);
+        if(symbol != NULL) {
+            printf("%s R%1d, %s", INSTR[opcode], (instruction >> 9) & 0b111,  symbol);
+        } else {
+            printf("%s R%1d, #%d", INSTR[opcode], (instruction >> 9) & 0b111, PCoffset9);
+        }
+
+        printf("\n");
+        return;
+    }
+}
--- simulator/simulator.h
+++ simulator/simulator.h
@@ -1,12 +1,18 @@
+#define MAX_SYMBOLS 255
+
 typedef struct{
-    __INT16_TYPE__ regfile[8];
-    __INT16_TYPE__ memory[65536]; // 2^16
-    __INT16_TYPE__ MDR;
+    __UINT16_TYPE__ regfile[8];
+    __UINT16_TYPE__ memory[65536]; // 2^16
+    __UINT16_TYPE__ MDR;
     __UINT16_TYPE__ MAR;
     __UINT16_TYPE__ PC;
     __UINT16_TYPE__ IR;
 
     //Symbol table
+    int numSymbols;
+
+    char* symbols[MAX_SYMBOLS];
+    __UINT16_TYPE__ symAddr[MAX_SYMBOLS];
 } CPU;
 
 CPU * create_CPU();
@@ -17,3 +23,8 @@ int load_binary(CPU* cpu, char* filename);
 
 int parse_filename(char* filename, char* bin_filename, char* sym_filename);
 __UINT16_TYPE__ le_to_be(__UINT16_TYPE__ input);
+__UINT16_TYPE__ get_sym_addr(CPU* lc3cpu, char* symName);
+char* get_addr_sym(CPU* lc3cpu, __UINT64_TYPE__ addr);
+void dumpMem(CPU* lc3cpu, __UINT16_TYPE__ addr);
+void printregs(CPU* lc3cpu);
+void print_instr(CPU* lc3cpu, __UINT16_TYPE__ memAddr);
